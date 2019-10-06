42d58542e090ce4906b24633cd69ed6e2291927c
VincentUTS <vincent.lay@student.uts.edu.au>
2019-10-06 21:34:33 +1100

    Creating base for main filter functions

--- filter.c
+++ filter.c
@@ -44,7 +44,7 @@ void select_filter (int items_size)
       switch(filter_selection)
 	{
 	case 1:
-
+	  filter_int_items (items_size);
 	  break;
 	case 2:
 
@@ -68,8 +68,8 @@ void select_filter (int items_size)
   return;
 }
 /*******************************************************************************
- * This function displays filtered entries of the database.
- * If input is '*', it will call a function to display all item entries.
+ * This function filters database entries according to the book ID.
+ * If input is '-1', it will call a function to display all item entries.
  * inputs:
  * - item_t* items: An array of size MAX_NUM_items containing all item entries
  * - int items_size: The number of items currently in the array item_t* items
@@ -77,18 +77,157 @@ void select_filter (int items_size)
  * outputs:
  * - None  
 *******************************************************************************/
-void display_sel_items (int items_size)
+void filter_int_items (int items_size)
 {
-  /* Input search term or asterisk (*) */
-  char search_input[MAX_ITEM_NAME_LEN + 1];
+  /* Input search term */
+  int search_input;
+
+  /* -1 not a valid book ID, so we will use this number to display all items*/
+  printf("Enter '-1' without quotes to display all items, or\n"
+	 "Enter search term>\n");
+  scanf("%d", &search_input);
+
+  /* Allow user to re-input search term until it is valid */
+  while (search_input < -1 || search_input > 32767 || search_input == 0)
+    {
+      printf("Invalid search term\n");
+      printf("Enter '-1' without quotes to display all items, or\n"
+	      "Enter search term>\n");
+      scanf("%d", &search_input);
+    }
+
+  /* If '-1' is input, call function display_all_items */
+  /* Also, if items_size == 0 (i.e. no items in database),
+  /* Call function display_all_items */
+  if (search_input == -1 || items_size == 0)
+    {
+      display_all_items (items_size);
+    }
+
+  else {
+    /* Checking if there are no matches between search term */
+    /* and with structure item_t's variables */
+
+    /* Placeholder (*.arrival_city, flight*) below! */
+    if ((strcmp(search_input, flight1.arrival_city) != 0) &&
+	(strcmp(search_input, flight2.arrival_city) != 0) &&
+	(strcmp(search_input, flight3.arrival_city) != 0) &&
+	(strcmp(search_input, flight4.arrival_city) != 0) &&
+	(strcmp(search_input, flight5.arrival_city) != 0)) {
+      printf("No flights\n");
+    }
+    /* Comparing search term with items in database */
+    /* If strcmp returns a '0', call display_item function */
+   if (strcmp(search_input, flight1.arrival_city) == 0) {
+     display_flight (flight1);
+   }
+   if (strcmp(search_input, flight2.arrival_city) == 0) {
+     display_flight (flight2);
+   }
+   if (strcmp(search_input, flight3.arrival_city) == 0) {
+     display_flight (flight3);
+   }
+   if (strcmp(search_input, flight4.arrival_city) == 0) {
+     display_flight (flight4);
+   }
+   if (strcmp(search_input, flight5.arrival_city) == 0) {
+     display_flight (flight5);
+   }
+  }
+
+  return;
+}
+
+/*******************************************************************************
+ * This function filters database entries according to ISBN.
+ * If input is '-1', it will call a function to display all item entries.
+ * inputs:
+ * - item_t* items: An array of size MAX_NUM_items containing all item entries
+ * - int items_size: The number of items currently in the array item_t* items
+ * - 
+ * outputs:
+ * - None  
+*******************************************************************************/
+void filter_long_items (int items_size)
+{
+  /* Input search term */
+  long search_input;
+  
+  printf("Enter search term>\n");
+  scanf("%li", search_input);
+
+  /* Allow user to re-input search term until it is valid */
+  while (search_input < -1 || search_input > 2147483647 || search_input == 0)
+    {
+      printf("Invalid search term\n");
+      printf("Enter '-1' without quotes to display all items, or\n"
+	      "Enter search term>\n");
+      scanf("%li", &search_input);
+    }
+
+  /* If '-1' is input, call function display_all_items */
+  /* Also, if items_size == 0 (i.e. no items in database),
+  /* Call function display_all_items */
+  if (search_input == -1 || items_size == 0) {
+    display_all_items (items_size);
+  }
+
+  else {
+    /* Checking if there are no matches between search term */
+    /* and with structure item_t's variables */
+
+    /* Placeholder (*.arrival_city, flight*) below! */
+    if ((strcmp(search_input, flight1.arrival_city) != 0) &&
+	(strcmp(search_input, flight2.arrival_city) != 0) &&
+	(strcmp(search_input, flight3.arrival_city) != 0) &&
+	(strcmp(search_input, flight4.arrival_city) != 0) &&
+	(strcmp(search_input, flight5.arrival_city) != 0)) {
+      printf("No flights\n");
+    }
+    /* Comparing search term with items in database */
+    /* If strcmp returns a '0', call display_tiem function */
+   if (strcmp(search_input, flight1.arrival_city) == 0) {
+     display_flight (flight1);
+   }
+   if (strcmp(search_input, flight2.arrival_city) == 0) {
+     display_flight (flight2);
+   }
+   if (strcmp(search_input, flight3.arrival_city) == 0) {
+     display_flight (flight3);
+   }
+   if (strcmp(search_input, flight4.arrival_city) == 0) {
+     display_flight (flight4);
+   }
+   if (strcmp(search_input, flight5.arrival_city) == 0) {
+     display_flight (flight5);
+   }
+  }
+
+  return;
+}
+
+/*******************************************************************************
+ * This function filters specified string entries within database.
+ * If input is '****', it will call a function to display all item entries.
+ * inputs:
+ * - item_t* items: An array of size MAX_NUM_items containing all item entries
+ * - int items_size: The number of items currently in the array item_t* items
+ * - 
+ * outputs:
+ * - None  
+*******************************************************************************/
+void filter_char_items (int items_size)
+{
+  /* Input search term */
+  char search_input[256];
   
   printf("Enter search term>\n");
   scanf("%s", search_input);
 
-  /* If '*' is input, call function display_all_items */
+  /* If '****' is input, call function display_all_items */
   /* Also, if items_size == 0 (i.e. no items in database),
   /* Call function display_all_items */
-  if (search_input[0] == '*' || items_size == 0) {
+  if (search_input[0] == '****' || items_size == 0) {
     display_all_items (items_size);
   }
 
