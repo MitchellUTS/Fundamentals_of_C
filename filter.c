ae64bae6a957bbad134268045c1627bd67f6920b
VincentUTS <vincent.lay@student.uts.edu.au>
2019-10-11 17:55:29 +1100

    Completed filter functions

--- filter.c
+++ filter.c
@@ -16,7 +16,6 @@
  * for the filter type, then calling a function 'display_item'
  * to search through database entries and print them.
  * inputs:
- * - int items_size: The number of items currently in the array item_t* items
  * - item_node_t* node: The head node of the database's linked list of items
  * outputs:
  * - None  
@@ -25,18 +24,16 @@ void select_filter (item_node_t* node)
 {
   /* Choice of filter user has selected */
   int filter_selection; 
-  /* Call function to get number of records within database */
-  /* int items_size = getNoOfRecords(); */
-  /* Placeholder value of 1 for testing */
-  int items_size = 1;
   /* Which case we are searching through for character data types */
   int char_search_case;
   /* Buffer array for checking input */
   char input_buffer[256];
-  
+
+  /* i.e. the node pointer points to nothing */
   if (node == NULL)
     {
       printf("No database loaded");
+      return;
     }
   else
     {
@@ -48,7 +45,6 @@ void select_filter (item_node_t* node)
       fgets(input_buffer, 256, stdin);
       if (sscanf(input_buffer, "%d", &filter_selection) == 1 )
 	;  /* Do nothing (no operation) */
-       /* scanf("%d", &filter_selection); */
 
       /* Force user to re-input selection until valid */
       while (filter_selection < 1 || filter_selection > 7)
@@ -65,27 +61,27 @@ void select_filter (item_node_t* node)
 	{
 	case 1:
 	  /* Book ID's */
-	  filter_int_items (items_size, node);
+	  filter_int_items (node);
 	  break;
 	case 2:
 	  /* Book ISBN's */
-	  filter_long_items (items_size, node);
+	  filter_long_items (node);
 	  break;
 	case 3:
 	  /* Book Titles */
-	  filter_char_items (items_size, char_search_case = 1, node);
+	  filter_char_items (char_search_case = 1, node);
 	  break;
 	case 4:
 	  /* Book Authors */
-	  filter_char_items (items_size, char_search_case = 2, node);
+	  filter_char_items (char_search_case = 2, node);
 	  break;
 	case 5:
 	  /* Book Types */
-	  filter_char_items (items_size, char_search_case = 3, node);
+	  filter_char_items (char_search_case = 3, node);
 	  break;
 	case 6:
 	  /* Book Categories */
-	  filter_char_items (items_size, char_search_case = 4, node);
+	  filter_char_items (char_search_case = 4, node);
 	  break;
 	}
      /* Continue the program until user types '7' to exit */
@@ -97,12 +93,11 @@ void select_filter (item_node_t* node)
  * This function filters database entries according to the book ID.
  * If input is '-1', it will call a function to display all item entries.
  * inputs:
- * - int items_size: The number of items currently in the array item_t* items
  * - item_node_t* node: The head node of the database's linked list of items
  * outputs:
  * - None  
 *******************************************************************************/
-void filter_int_items (int items_size, item_node_t* node)
+void filter_int_items (item_node_t* node)
 {
   /* Input search term */
   int search_input;
@@ -132,7 +127,7 @@ void filter_int_items (int items_size, item_node_t* node)
       break;
     }
 
-  if (search_input == -1 || items_size == 0)
+  if (search_input == -1)
     {
       printf("Displaying all items\n");
       /* Call display_database function */
@@ -170,12 +165,11 @@ void filter_int_items (int items_size, item_node_t* node)
  * This function filters database entries according to ISBN.
  * If input is '-1', it will call a function to display all item entries.
  * inputs:
- * - int items_size: The number of items currently in the array item_t* items
  * - item_node_t* node: The head node of the database's linked list of items
  * outputs:
  * - None  
 *******************************************************************************/
-void filter_long_items (int items_size, item_node_t* node)
+void filter_long_items (item_node_t* node)
 {
   /* Input search term */
   long search_input;
@@ -216,7 +210,7 @@ void filter_long_items (int items_size, item_node_t* node)
       printf("Searching for '%li' under Book 'ISBNs'\n", search_input);
       while (node != NULL)
 	{
-	  /* Check if item ID for structure within current node... */
+	  /* Check if item ISBN for structure within current node... */
 	  /* ...is equal to search_input */
 	  if (((item_t*)(current->item_data))->ISBN == search_input)
 	    {
@@ -242,20 +236,23 @@ void filter_long_items (int items_size, item_node_t* node)
  * This function filters specified string entries within database.
  * If input is '****', it will call a function to display all item entries.
  * inputs:
- * - int items_size: The number of items currently in the array item_t* items
  * - int char_search_case: An integer corresponding to each string-type...
  * ...member of items_t array
  * - item_node_t* node: The head node of the database's linked list of items
  * outputs:
  * - None  
 *******************************************************************************/
-void filter_char_items (int items_size, int char_search_case, item_node_t* node)
+void filter_char_items (int char_search_case, item_node_t* node)
 {
   /* Input search term */
   char search_input[256];
   /* Storage for string to trigger wildcard search */
   char str1[6];
-
+  /* Pointer to store value returned by strstr function, for later checking */
+  char *str_chkp;
+  /* Number matches for search */
+  int matches = 0;
+  
   strcpy(str1, "****");
   
   printf("Enter search term>\n");
@@ -271,20 +268,108 @@ void filter_char_items (int items_size, int char_search_case, item_node_t* node)
   }
 
   else if (char_search_case == 1)
+    /* Search case for titles */
     {
     printf("Searching for '%s' under Book 'Titles'\n", search_input);
+    while (node != NULL)
+	{
+	  /* Check if substring of search term inside 'title' */
+	  *str_chkp = strstr(((item_t*)(current->item_data))->title,
+			     search_input);
+	  /* strstr returns NULL if no substring found */
+	  if (str_chkp != NULL)
+	    {
+	      /* Call function to display items for current node */
+	      display_item(node);
+	      /* Increase item match count */
+	      matches = matches + 1;
+	    }
+	  /* Move to next linked list node */
+	  node = node->next;
+	}
+    if (matches == 0)
+	{
+	  /* Print no matches if there were no matches */
+	  printf("No exact matches found for '%s'\n", search_input);
+	}
     }
   else if (char_search_case == 2)
+    /* Search case for authors */
     {
     printf("Searching for '%s' under 'Author'\n", search_input);
+        while (node != NULL)
+	{
+	  /* Check if substring of search term inside 'author' */
+	  *str_chkp = strstr(((item_t*)(current->item_data))->author,
+			     search_input);
+	  /* strstr returns NULL if no substring found */
+	  if (str_chkp != NULL)
+	    {
+	      /* Call function to display items for current node */
+	      display_item(node);
+	      /* Increase item match count */
+	      matches = matches + 1;
+	    }
+	  /* Move to next linked list node */
+	  node = node->next;
+	}
+    if (matches == 0)
+	{
+	  /* Print no matches if there were no matches */
+	  printf("No exact matches found for '%s'\n", search_input);
+	}
     }
   else if (char_search_case == 3)
+    /* Search case for types */
     {
     printf("Searching for '%s' under Book 'Type'\n", search_input);
+        while (node != NULL)
+	{
+	  /* Check if substring of search term inside 'type' */
+	  *str_chkp = strstr(((item_t*)(current->item_data))->type,
+			     search_input);
+	  /* strstr returns NULL if no substring found */
+	  if (str_chkp != NULL)
+	    {
+	      /* Call function to display items for current node */
+	      display_item(node);
+	      /* Increase item match count */
+	      matches = matches + 1;
+	    }
+	  /* Move to next linked list node */
+	  node = node->next;
+	}
+    if (matches == 0)
+	{
+	  /* Print no matches if there were no matches */
+	  printf("No exact matches found for '%s'\n", search_input);
+	}
     }
   else if (char_search_case == 4)
+    /* Search case for categories */
     {
     printf("Searching for '%s' under Book 'Category'\n", search_input);
+        while (node != NULL)
+	{
+	  /* Check if substring of search term inside 'category' */
+	  *str_chkp = strstr(((item_t*)(current->item_data))->category,
+			     search_input);
+	  /* strstr returns NULL if no substring found */
+	  if (str_chkp != NULL)
+	    {
+	      /* Call function to display items for current node */
+	      display_item(node);
+	      /* Increase item match count */
+	      matches = matches + 1;
+	    }
+	  /* Move to next linked list node */
+	  node = node->next;
+	}
+    if (matches == 0)
+	{
+	  /* Print no matches if there were no matches */
+	  printf("No exact matches found for '%s'\n", search_input);
+	}
     }
   else
     {
